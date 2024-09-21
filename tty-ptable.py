95488e361cc18fa41df623545236926cc297e434
sheeb1 <sheeb1@proton.me>
2024-09-21 17:55:09 -0400

    Create tty-ptable.py
    Started hydrogen info

---
+++ tty-ptable.py
@@ -0,0 +1,12 @@
+def main():
+  print(
+
+#I dont know how this works so im guessing
+  def Hydrogen():
+    Print("Element name: Hydrogen")
+    print("Element symbol: H")
+    print("atomic mass (amu): 1.008u")
+    print("Series: Reactive Nonmetals")
+    print("Common isotopes: Hydrogen-1, Hydrogen-2")
+    print("Radioactive isotopes: Hydrogen-3 (β− decay), Hydrogen-4, Hydrogen-5, Hydrogen-6, Hydrogen-7 (neutron emmission)
+    print("
