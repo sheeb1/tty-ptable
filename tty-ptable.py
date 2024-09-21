3e201b5a9185bf6b13b90b7b0e8d1b62980132c3
sheeb1 <sheeb1@proton.me>
2024-09-21 18:28:31 -0400

    Update tty-ptable.py

--- tty-ptable.py
+++ tty-ptable.py
@@ -8,5 +8,5 @@ def main():
     print("atomic mass (amu): 1.008u")
     print("Series: Reactive Nonmetals")
     print("Common isotopes: Hydrogen-1, Hydrogen-2")
-    print("Radioactive isotopes: Hydrogen-3 (β− decay), Hydrogen-4, Hydrogen-5, Hydrogen-6, Hydrogen-7 (neutron emmission)
+    print("Radioactive isotopes: Hydrogen-3 (β− decay), Hydrogen-4, Hydrogen-5, Hydrogen-6, Hydrogen-7 (neutron emmission)")
     print("
