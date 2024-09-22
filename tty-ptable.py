f3d5b2ab76540bf5dc8fdd1828abe95b6f89bac4
sheeb1 <sheeb1@proton.me>
2024-09-21 23:16:32 -0400

    Update tty-ptable.py
    Added Helium

--- tty-ptable.py
+++ tty-ptable.py
@@ -5,8 +5,19 @@ def main():
   def Hydrogen():
     print("Element name: Hydrogen")
     print("Element symbol: H")
+    print("Atomic Number: 1")
     print("atomic mass (amu): 1.008u")
     print("Series: Reactive Nonmetals")
-    print("Common isotopes: Hydrogen-1, Hydrogen-2")
-    print("Radioactive isotopes: Hydrogen-3 (β− decay), Hydrogen-4, Hydrogen-5, Hydrogen-6, Hydrogen-7 (neutron emmission)")
-    print("
+    print("Common isotopes: Hydrogen-1 (99.972%), Hydrogen-2 (0.001%)")
+    print("Radioactive isotopes: Hydrogen-3 (trace) (β− decay), Hydrogen-4, Hydrogen-5, Hydrogen-6, Hydrogen-7 (neutron emmission)")
+    print("Electronegativity: 2.20")
+
+def Helium():
+  print("Element name: Helium")
+    print("Element symbol: He")
+    print("Atomic Number: 2")
+    print("atomic mass (amu): 4.0026u")
+    print("Series: Noble Gases")
+    print("Common isotopes: Helium-3 (0.0002%), Helium-4 (99.9998%)")
+    print("Radioactive isotopes: Helium-2 (proton emmission), Helium-5, Helium-7, Helium-9, Helium-10 (neutron emmission), Helium-6, Helium-8 (β− decay)")
+    print("Electronegativity: N/A")
