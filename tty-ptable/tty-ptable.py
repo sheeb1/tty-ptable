2f4d5fe0f7f866293e09df19b93f3ebfbb66735a
sheeb1 <sheeb1@proton.me>
2024-09-22 23:05:45 -0400

    Update tty-ptable.py

--- tty-ptable/tty-ptable.py
+++ tty-ptable/tty-ptable.py
@@ -1,53 +1,42 @@
 def main():
   print(
 
-#I dont know how this works so im guessing
-
-###HyderogenASCII                                                                                                                                                                                
-#                                               ++++++                                               
-#                                              ++++++++                                              
-#                                       ++++   ++++++++   ++++                                       
-#                                  +++           ++++           +++                                  
-#                               ++                                  ++                               
-#                            ++                                        ++                            
-#                         ++                                              ++                         
-#                       ++                                                  ++                       
-#                      +                        %%%%%%                        +                      
-#                    ++                  %%%%::::::::::::%%%%                  ++                    
-#                   +                %%%::::::::::::::::::::::%%%                +                   
-#                  +               %%::::::::::::::::::::::::::::%%               +                  
-#                 +              %%::::::::::::::::::::::::::::::::%%              +                 
-#                +             %%::::::::::::::::::::::::::::::::::::%%             +                
-#               +             %::::::::::::::::::::::::::::::::::::::::%             +               
-#              +             %::::::::::::::::::::::::::::::::::::::::::%             +              
-#              +            %::::::::::::::::::::::::::::::::::::::::::::%            +              
-#             +            %::::::::::::::::::::::::::::::::::::::::::::::%            +             
-#             +           %%::::::::::::::::::::::::::::::::::::::::::::::%%           +             
-#            +            %:::::::::::::::::@@@@@@::@@@@@@:::::::::::::::::%            +            
-#            +            %:::::::::::::::::::@@+::::+@@:::::::::::::::::::%            +            
-#            +           %%:::::::::::::::::::@@+::::+@@:::::::::::::::::::%%           +            
-#            +           %%:::::::::::::::::::@@@@@@@@@@:::::::::::::::::::%%           +            
-#            +           %%:::::::::::::::::::@@+::::+@@:::::::::::::::::::%%           +            
-#            +            %:::::::::::::::::::@@+::::+@@:::::::::::::::::::%            +            
-#            +            %::::::::::::::::::@@@@-::-@@@@::::::::::::::::::%            +            
-#             +           %%::::::::::::::::::::::::::::::::::::::::::::::%%           +             
-#             +            %::::::::::::::::::::::::::::::::::::::::::::::%            +             
-#              +            %::::::::::::::::::::::::::::::::::::::::::::%            +              
-#              +             %::::::::::::::::::::::::::::::::::::::::::%             +              
-#               +             %::::::::::::::::::::::::::::::::::::::::%             +               
-#                +             %%::::::::::::::::::::::::::::::::::::%%             +                
-#                 +              %%::::::::::::::::::::::::::::::::%%              +                 
-#                  +               %%::::::::::::::::::::::::::::%%               +                  
-#                   +                %%%::::::::::::::::::::::%%%                +                   
-#                    ++                  %%%%::::::::::::%%%%                  ++                    
-#                      +                        %%%%%%                        +                      
-#                       ++                                                  ++                       
-#                         ++                                              ++                         
-#                            ++                                        ++                            
-#                               ++                                  ++                               
-#                                  +++                          +++                                  
-#                                       ++++              ++++      
-#                                           ++++++++++++++
+elements = {
+  'H' = {
+    'name': 'Hydrogen',
+    'atomic_number': 1,
+    'atomic_mass': '1.008 u',
+    'series': 'Reactive nonmetals',
+    'common_isotopoes': 'Hydrogen-1 (99.972%), Hydrogen-2 (0.001%)',
+    'radioactive_isotopes': 'Hydrogen-3 (trace) (β− decay), Hydrogen-4, Hydrogen-5, Hydrogen-6, Hydrogen-7 (neutron emmission)',
+    'electronegativity': '2.20',
+    'electron_configuration': '1s1'
+  }
+  'He' = {
+    'name': 'Helium',
+    'atomic_number': 2,
+    'atomic_mass': '4.0026 u',
+    'series': 'Noble gases',
+    'common_isotopoes': 'Helium-3 (0.0002%), Helium-4 (99.9998%)',
+    'radioactive_isotopes': 'Helium-2 (proton emmission), Helium-5, Helium-7, Helium-9, Helium-10 (neutron emmission), Helium-6, Helium-8 (β− decay)',
+    'electronegativity': 'N/A',
+    'electron_configuration': '1s2'
+  }
+  'Li' = {
+    'name': 'Lithium',
+    'atomic_number': 3,
+    'atomic_mass': '6.94 u',
+    'series': 'Alkali metals',
+    'common_isotopoes': 'Lithium-6 (7.59%), Lithium-7 (92.41%)',
+    'radioactive_isotopes': 'Lithium-3, Lithium-4, Lithium-5 (proton emmission) Lithium-8, Lithium-9, Lithium-11 (β− decay), Lithium-10, Lithium-12 (neutron emmission)',
+    'electronegativity': '0.98',
+    'electron_configuration': '1s2 2s1'
+  }
+  'Be' = {
+    
+    
+  }
+}
 def Hydrogen():
     print("Element name: Hydrogen")
     print("Element symbol: H")
